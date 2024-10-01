3ae609e9aa1f551ded8bfc8a30aa908e3a86f3bb
EdiYt <erickvillaseor@yahoo.com.mx>
2024-09-30 22:47:11 -0600

    Ya se pueden checar los alumnos que tiene asignado cada profesor

--- controllers/profesoresController.js
+++ controllers/profesoresController.js
@@ -14,3 +14,20 @@ exports.obtenerGruposAsignados = (req, res) => {
         res.json(results);
     });
 };
+
+// Obtener alumnos de los grupos asignados a un profesor
+exports.obtenerAlumnosDeProfesor = (req, res) => {
+    const profesor = req.params.nombre;
+
+    db.query(`
+        SELECT grupos.nombre AS grupoNombre, alumnos.nombre AS alumnoNombre
+        FROM grupos
+        JOIN grupo_alumnos ON grupos.id = grupo_alumnos.grupo_id
+        JOIN alumnos ON grupo_alumnos.alumno_id = alumnos.id
+        WHERE grupos.profesor = ?
+    `, [profesor], (err, results) => {
+        if (err) return res.status(500).send('Error al obtener los alumnos del profesor');
+        if (results.length === 0) return res.status(404).send('No se encontraron alumnos para este profesor');
+        res.json(results);
+    });
+};
--- routes/profesores.js
+++ routes/profesores.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const profesoresController = require('../controllers/profesoresController');
 
 router.get('/:nombre/grupos', profesoresController.obtenerGruposAsignados);
+router.get('/:nombre/alumnos', profesoresController.obtenerAlumnosDeProfesor);
 
 module.exports = router;
